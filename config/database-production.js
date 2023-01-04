0422d87279033e0eddaee58a72590e26ccebc11c
NaraVictor <naravictor@live.com>
2023-01-04 08:07:10 +0000

    dev & prod configs

--- config/database-production.js
+++ config/database-production.js
@@ -2,13 +2,6 @@ const CryptoJS = require( "crypto-js" );
 const { key } = require( '../src/util/enums' )
 
 module.exports = {
-  development: {
-    username: "postgres",
-    password: "admin",
-    database: "spearPOS_dev",
-    host: "localhost",
-    dialect: "postgres"
-  },
   production: {
     username: CryptoJS.AES.decrypt( process.env.DB_USER, key ).toString( CryptoJS.enc.Utf8 ),
     password: CryptoJS.AES.decrypt( process.env.DB_PASSWORD, key ).toString( CryptoJS.enc.Utf8 ),
